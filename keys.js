79dd393c09bf0f2b4bee0e10c90fc6a945dfbc7b
omer1228 <oigba2105@withs.org>
2016-06-07 13:54:27 +0000

    2 bongos

--- keys.js
+++ keys.js
@@ -1,33 +1,33 @@
 // keys: w=119 s=115 e=101 d=100, k=107 i=105 o=111 l=108 //
 
-$("body").keypress(function(event) {
+("body").keypress(function(event) {
     console.log(event);
     if (event.which === 119) {
-        $('#w')[0].play();
+        ('#w')[0].play();
     }
     if (event.which === 119) {
-        $('#s')[0].play();
+        ('#s')[0].play();
     }
     if (event.which === 101) {
-        $('#e')[0].play();
+        ('#e')[0].play();
     }
     if (event.which === 100) {
-        $('#d')[0].play();
+        ('#d')[0].play();
     }
     if (event.which === 107) {
-        $('#k')[0].play();
+        ('#k')[0].play();
     }
     if (event.which === 105) {
-        $('#i')[0].play();
+        ('#i')[0].play();
     }
     if (event.which === 111) {
-        $('#o')[0].play();
+        ('#o')[0].play();
     }
     if (event.which === 108) {
-        $('#l')[0].play();
+        ('#l')[0].play();
     }
 
 
 });
 
-console.log($('audio'));
+console.log(('audio'));
